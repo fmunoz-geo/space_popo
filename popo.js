334bcb6dbfb8d4fb375ab6b32d74c4b151b83668
Francisco Muñoz <fmunoz.geo@yahoo.com>
2024-07-22 18:44:26 +0200

    Update

--- popo.js
+++ popo.js
@@ -165,10 +165,10 @@ function drawExplosion(x, y) {
 }
 
 function checkCollision(obj1, obj2) {
-    return obj1.x < obj2.x + obj2.width &&
-           obj1.x + obj1.width > obj2.x &&
-           obj1.y < obj2.y + obj2.height &&
-           obj1.y + obj1.height > obj2.y;
+    return obj1.x + 5 < obj2.x + obj2.width &&
+           obj1.x + obj1.width > obj2.x + 5 &&
+           obj1.y + 5 < obj2.y + obj2.height &&
+           obj1.y + obj1.height > obj2.y + 5;
 }
 
 function updateScore() {
